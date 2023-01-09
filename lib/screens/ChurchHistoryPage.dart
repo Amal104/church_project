a93e9d543c3f2f973136106c9a3374f8ff126a97
amal <amalka@taski.in>
2023-01-09 11:08:19 -1200

    commit

--- lib/screens/ChurchHistoryPage.dart
+++ lib/screens/ChurchHistoryPage.dart
@@ -1,3 +1,4 @@
+import 'package:church/values/Strings.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
@@ -13,72 +14,109 @@ class ChurchHistory extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(
-              onPressed: () {
-                Get.back();
-              },
-              icon: const FaIcon(
-                FontAwesomeIcons.chevronLeft,
-                color: AppColor.grey400,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: width / 25,
-              ),
-              child: Text(
-                "History of\nChurch",
-                style: GoogleFonts.inter(
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                  color: AppColor.grey600,
-                  letterSpacing: 1.0,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Get.back();
+                },
+                icon: const FaIcon(
+                  FontAwesomeIcons.chevronLeft,
+                  color: AppColor.grey400,
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: width / 30),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        height: height / 5,
-                        width: width / 2.28,
-                        decoration: BoxDecoration(
-                            color: AppColor.purpleShadeWithOpec,
-                            borderRadius: BorderRadius.circular(50)),
-                      ),
-                      Container(
-                        height: height / 5,
-                        width: width / 2.28,
-                        decoration: BoxDecoration(
-                            color: AppColor.purpleShadeWithOpec,
-                            borderRadius: BorderRadius.circular(50)),
-                      ),
-                    ],
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: width / 25,
+                ),
+                child: Text(
+                  "History of\nChurch",
+                  style: GoogleFonts.inter(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: AppColor.grey600,
+                    letterSpacing: 1.0,
                   ),
-                  SizedBox(
-                    height: height * 0.01,
-                  ),
-                  Container(
-                    height: height * 0.5,
-                    width: width * 0.95,
-                    decoration: BoxDecoration(
-                        color: AppColor.purpleShadeWithOpec,
-                        borderRadius: BorderRadius.circular(50)),
-                  ),
-                ],
+                ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: width / 30),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: width / 12),
+                          height: height / 4,
+                          width: width / 2.28,
+                          decoration: BoxDecoration(
+                              color: AppColor.lightGreyShade,
+                              borderRadius: BorderRadius.circular(50)),
+                          child: Center(
+                              child: Text(
+                            "Founded\n\nc. AD 593;\n1430 years ago",
+                            style: GoogleFonts.inter(
+                              fontSize: 21,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.darkpurpleShadev2,
+                            ),
+                          )),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: width / 30),
+                          height: height / 4,
+                          width: width / 2.28,
+                          decoration: BoxDecoration(
+                              color: AppColor.lightGreyShade,
+                              borderRadius: BorderRadius.circular(50)),
+                          child: Center(
+                              child: Text(
+                            "Denomination\n\nSyro-Malabar Catholic Church",
+                            style: GoogleFonts.inter(
+                              fontSize: 21,
+                              fontWeight: FontWeight.w600,
+                              color: AppColor.darkPinkShade,
+                            ),
+                          )),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: height * 0.01,
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: width / 30,
+                        vertical: height / 20,
+                      ),
+                      width: width * 0.95,
+                      decoration: BoxDecoration(
+                          color: AppColor.lightGreyShade,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Text(
+                        AppStrings.achansMessage,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.inter(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w600,
+                            color: AppColor.darkpurpleShadev2,
+                            height: 1.5),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 100.0,
+              ),
+            ],
+          ),
         ),
       ),
     );
